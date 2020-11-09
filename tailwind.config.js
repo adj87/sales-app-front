15150ebcfc3c618cd8cfb3267dfedc6b004f66be
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-11-09 20:13:43 +0100

    first config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      blue: {
+        100: '#f7fafc',
+        // ...
+        900: '#1a202c',
+      },
+    },
+    variants: {},
+    plugins: [],
+  },
+};
