297bb5e019e33041e4d4e975a6deec33f4e4350a
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-11-10 18:40:46 +0100

    more adjustment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div className="bg-main-blue flex px-4 w-full justify-center shadow-md">
-   
-    <span className="px-4 py-6 text-main-blue-500">Link 1</span>
-    <span className="px-4 py-6 text-main-blue-500">Link 1</span>
-    <span className="px-4 py-6 text-main-blue-500">Link 1</span>
-
-
-    <span className="absolute right-0 px-4 py-6">Logout</span>
-  </div>;
+  return (
+    <div className="bg-blue-dark flex px-4 w-full justify-center shadow-md">
+      <span className="px-4 py-6 text-blue-light">Link 1</span>
+      <span className="px-4 py-6 text-blue-light">Link 1</span>
+      <span className="px-4 py-6 text-white font-extrabold">Link 1</span>
+      <span className="absolute right-0 px-4 py-6 text-blue-light">Logout</span>
+    </div>
+  );
 };
 
 export default Navbar;
