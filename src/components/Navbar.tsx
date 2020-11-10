a4d094ba7a267ce3d2a119ef408834bc29f39f37
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-11-10 14:20:45 +0100

    prototype navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div className="bg-main-blue px-4 w-full">Hola</div>;
+  return <div className="bg-main-blue flex px-4 w-full justify-center shadow-md">
+   
+    <span className="px-4 py-6 text-main-blue-500">Link 1</span>
+    <span className="px-4 py-6 text-main-blue-500">Link 1</span>
+    <span className="px-4 py-6 text-main-blue-500">Link 1</span>
+
+
+    <span className="absolute right-0 px-4 py-6">Logout</span>
+  </div>;
 };
 
 export default Navbar;
