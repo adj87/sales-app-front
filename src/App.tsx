b736bbcc633e554f427b76c1ec8431d1542fb26a
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-11-10 21:05:32 +0100

    some progress with table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './css/dist/index.css';
 import Navbar from './components/Navbar';
+import { Table } from './components/Table';
 
 function App() {
-  return <Navbar />;
+  const columns = [
+    { accessor: 'unidades', header: 'Unidades' },
+    { accessor: 'iva', header: 'Iva' },
+    { accessor: 'precio', header: 'Esto' },
+    { accessor: 'producto', header: 'Producto' },
+  ];
+
+  const data = [{ unidades: 20, producto: 'Fre' }];
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto px-4">
+        <div className="w-full my-16">
+          <Table columns={columns} data={data} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+interface Column {
+  accessor: String;
+  header: String;
+}
+
+interface TableProps {
+  columns: Array<Column>;
+  data: Array<any>;
+}
+
+type Sort = String | null;
+
+export const Table = ({ columns, data }: TableProps) => {
+  const initialSort = columns[0]?.accessor;
+  const [sort, setSort] = useState<Sort>(initialSort);
+
+  return (
+    <div className="w-full flex-row">
+      <Options />
+      <table className="w-full">
+        <thead>
+          <tr>
+            {columns.map((el: Column) => {
+              const color = sort === el.accessor ? 'text-white font-bold' : 'text-blue-light';
+              return (
+                <td
+                  className={`bg-blue-dark ${color} px-5 py-1 text-center`}
+                  onClick={() => setSort(el.accessor)}
+                >
+                  {el.header}
+                </td>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((el: any) => (
+            <tr className="border-b-2 border-blue-light ">
+              {columns.map((col: any) => (
+                <td className="bg-white text-grey-600 text-center py-2">{el[col.accessor]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Options = () => (
+  <div className="flex-row">
+    <span
+      style={{ width: '6px', height: '6px', borderRadius: '100%', display: 'inline-block' }}
+      className="bg-blue-dark my-1"
+    />
+    <span
+      style={{ width: '6px', height: '6px', borderRadius: '100%', display: 'inline-block' }}
+      className="bg-blue-dark my-1"
+    />
+    <span
+      style={{ width: '6px', height: '6px', borderRadius: '100%', display: 'inline-block' }}
+      className="bg-blue-dark my-1"
+    />
+  </div>
+);
