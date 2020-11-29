6b2ca642fd45a0eea1e6363b2a965a743d32220a
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-11-29 15:03:53 +0100

    add modalsize props

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,74 +1,64 @@
 import React from 'react';
 import Button from './Button';
 
-export default function Modal() {
-  const [showModal, setShowModal] = React.useState(true);
-  return (
+interface ModalProps {
+  size?: 'md' | 'lg';
+  open: boolean;
+  onCancel: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onConfirm: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function Modal({ size, open, onCancel, onConfirm }: ModalProps) {
+  const modalSize = size === 'lg' ? 'max-w-5xl' : 'max-w-3xl';
+  return open ? (
     <>
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-        type="button"
-        style={{ transition: 'all .15s ease' }}
-        onClick={() => setShowModal(true)}
-      >
-        Open regular modal
-      </button>
-      {showModal ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-grey-300 rounded-t">
-                  <h2 className="text-2xl text-primary-dark text-center font-semibold">
-                    Modal Title
-                  </h2>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-gray-600 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main thing people are
-                    controlled by! Thoughts- their perception of themselves! They're slowed down by
-                    their perception of themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-between p-6 border-t border-solid border-grey-300 rounded-b">
-                  <div className="px-8 w-full">
-                    <Button
-                      text={'cancelar'}
-                      color="secondary"
-                      outline
-                      size="block"
-                      onClick={() => setShowModal(false)}
-                    />
-                  </div>
-                  <div className="px-8 w-full">
-                    <Button
-                      text={'aceptar'}
-                      color="secondary"
-                      size="block"
-                      style={{ transition: 'all .15s ease' }}
-                      onClick={() => setShowModal(false)}
-                    />
-                  </div>
-                </div>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className={`relative w-auto my-6 mx-auto ${modalSize} px-2`}>
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-between p-5 border-b border-solid border-grey-300 rounded-t">
+              <h2 className="text-2xl text-primary-dark text-center font-semibold">Modal Title</h2>
+              <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none">
+                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            {/*body*/}
+            <div className="relative p-6 flex-auto">
+              <p className="my-4 text-gray-600 text-lg leading-relaxed">
+                I always felt like I could do anything. That’s the main thing people are controlled
+                by! Thoughts- their perception of themselves! They're slowed down by their
+                perception of themselves. If you're taught you can’t do anything, you won’t do
+                anything. I was taught I could do everything.
+              </p>
+            </div>
+            {/*footer*/}
+            <div className="flex items-center justify-between p-6 border-t border-solid border-grey-300 rounded-b">
+              <div className="px-8 w-full">
+                <Button
+                  text={'cancelar'}
+                  color="secondary"
+                  outline
+                  size="block"
+                  onClick={onCancel}
+                />
+              </div>
+              <div className="px-8 w-full">
+                <Button
+                  text={'aceptar'}
+                  color="secondary"
+                  size="block"
+                  style={{ transition: 'all .15s ease' }}
+                  onClick={onConfirm}
+                />
               </div>
             </div>
           </div>
-          <div className="opacity-50 fixed inset-0 z-40 bg-primary-light"></div>
-        </>
-      ) : null}
+        </div>
+      </div>
+      <div className="opacity-50 fixed inset-0 z-40 bg-primary-light"></div>
     </>
-  );
+  ) : null;
 }
