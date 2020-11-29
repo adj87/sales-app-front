686141a8e1cc953fd156bfdcd790e6b1d17bc8df
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-11-29 11:40:22 +0100

    some modification in buttons to fit modal

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 interface ButtonProps {
   text: String;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   color: String;
+  size?: String;
   className?: String;
+  style?: CSSProperties;
 }
 
-const Button = ({ text, onClick, color, className }: ButtonProps) => {
+const Button = ({ text, onClick, color, className, style, size }: ButtonProps) => {
+  const cssSize = getCssSize(size);
   return (
     <button
-      className={`bg-${color}-dark hover:bg-${color}-main text-white font-bold py-0 px-6 rounded ${className}`}
+      style={style ?? {}}
+      className={`bg-${color}-dark hover:bg-${color}-main text-white font-bold ${cssSize} rounded ${className} capitalize`}
       onClick={onClick}
     >
       {text}
     </button>
   );
+};
+
+const getCssSize = (size: String | undefined) => {
+  switch (size) {
+    case 'sm':
+      return 'py-0 px-6';
+    case 'block':
+      return 'w-full p-1';
+    default:
+      return 'py-2 px-6';
+  }
 };
 
 export default Button;
