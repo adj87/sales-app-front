f3bd11b3e127975b7a36497b0015a248c59cb244
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-12-09 08:18:50 +0100

    add null to any IOrder props

--- src/app/Orders/duck/types/Order.ts
+++ src/app/Orders/duck/types/Order.ts
@@ -1,17 +1,17 @@
 export interface IOrder {
-  id: number;
-  address: string;
-  fiscal_id: string;
-  zip_code: string;
-  date: string;
-  delivery_date: string;
-  total_net: number;
-  total_taxes: number;
-  total: number;
-  surcharge: number;
-  customer_id: number;
-  customer_name: string;
-  green_point: number;
-  customer_route_id: number;
-  type: string;
+  id: number | null;
+  address: string | null;
+  fiscal_id: string | null;
+  zip_code: string | null;
+  date: string | null;
+  delivery_date: string | null;
+  total_net: number | null;
+  total_taxes: number | null;
+  total: number | null;
+  surcharge: number | null;
+  customer_id: number | null;
+  customer_name: string | null;
+  green_point: number | null;
+  customer_route_id: number | null;
+  type: string | null;
 }
