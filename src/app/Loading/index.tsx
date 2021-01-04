b75dc3b5d805eb48464bf1dce902332c3e1bbe70
Dr Jauregui <albertodomjauregui@hotmail.com>
2021-01-04 08:07:20 +0100

    place loading in LayerOutOfRoot component

--- src/app/Loading/index.tsx
+++ src/app/Loading/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BarLoader } from 'react-spinners';
 import { AppStoreInterface } from '../../store/AppStoreInterface';
 import { connect } from 'react-redux';
+import LayerOutOfRoot from '../../components/Modal/Layer';
 
 interface LoadingProps {
   loading?: boolean;
@@ -11,14 +12,14 @@ const Loading = ({ loading }: LoadingProps) => {
   return (
     <>
       {loading && (
-        <>
+        <LayerOutOfRoot>
           <div
-            className={`w-full h-full fixed flex justify-center items-center z-10 left-0 top-0 opacity-50 fixed bg-blur-lg  bg-primary-light`}
+            className={`w-full h-full fixed flex justify-center items-center z-100 left-0 top-0 opacity-50 fixed `}
           />
           <div className="z-20 fixed w-full h-full justify-center items-center top-0 left-0 flex">
             <BarLoader height={'10'} width="400" color={'orange'} loading={loading} />
           </div>
-        </>
+        </LayerOutOfRoot>
       )}
     </>
   );
