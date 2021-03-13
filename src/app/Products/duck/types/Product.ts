cdf6f6c713468982bb5ba4030c097058fddc345a
jauregui_ind <albertodomjauregui@hotmail.com>
2021-03-13 12:54:03 +0100

    redesign Iproduct

--- src/app/Products/duck/types/Product.ts
+++ src/app/Products/duck/types/Product.ts
@@ -1,8 +1,24 @@
 export interface IProduct {
+  // product
   id: string;
   name: string;
   cost: number;
   green_point_amount: number;
   units_per_box: number;
   capacity: number;
+  weight: number;
+
+  // box
+  box_width: number;
+  box_height: number;
+  box_length: number;
+  box_weight: number;
+  box_capacity: number;
+
+  // pallet
+  pallet_boxes: number;
+  pallet_base: number;
+  pallet_weight: number;
+  pallet_capacity: number;
+  pallet_height: number;
 }
