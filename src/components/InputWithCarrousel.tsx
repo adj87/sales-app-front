803ab64234a507b234530ff309b86e0f888ff59e
jauregui_ind <albertodomjauregui@hotmail.com>
2021-03-13 12:26:55 +0100

    add logistic as component

--- src/components/InputWithCarrousel.tsx
+++ src/components/InputWithCarrousel.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
-import { useTranslation } from 'react-i18next';
 import { IProduct } from '../app/Products/duck/types/Product';
-import Button from './Button';
+import { LogisticSheet } from '../components/LogisticSheet';
 import { DEFAULT_IMAGE_URL } from '../constants';
 import LayerOutOfRoot from './Modal/Layer';
 import { getPhpBackHostUrl } from '../utils';
@@ -69,7 +68,7 @@ const InputWithCarrousel = ({ data, onChange, value }: InputWithCarrouselProps) 
         <div className="w-full"></div>
       </div>
       {showInMiddle !== null && (
-        <ProductDetail
+        <LogisticSheet
           product={showInMiddle}
           onClose={() => setShowInMiddle(null)}
           onAdd={() => {
@@ -80,61 +79,6 @@ const InputWithCarrousel = ({ data, onChange, value }: InputWithCarrouselProps) 
         />
       )}
     </>
-  );
-};
-
-interface ProductDetailProps {
-  product: IProduct;
-  onClose: Function;
-  onAdd: Function;
-}
-
-const ProductDetail = ({ product, onClose, onAdd }: ProductDetailProps) => {
-  const { t } = useTranslation();
-  const Line = ({ label, title }: { label: string; title: string }) => {
-    return (
-      <div className="flex flex-row justify-between py-2">
-        <span className="text-grey-900  ">{t(label)}</span>
-        <span className="text-grey-500">{title}</span>
-      </div>
-    );
-  };
-  return (
-    <LayerOutOfRoot className="modalclass fixed w-full h-full z-50 top-0 left-0 bg-white">
-      <div className="flex w-full lg:w-2/3 justify-center items-center m-auto" style={{ height: '90%' }}>
-        <div className="flex-1 flex justify-center">
-          <img className="w-full lg:w-2/3 mt-10" src={`${imgUrl(product.id)}`} />
-        </div>
-        <div className="flex-1 flex flex-col p-5">
-          <p className="text-bold text-primary-dark text-center p-5 uppercase font-bold text-xl">{product.name}</p>
-          <div className="px-10 xl:px-20">
-            <p className="text-center text-primary-main uppercase font-bold">{t('commons.bottle-details')}</p>
-            <Line label="commons.bar-code" title={'848958965841'} />
-            <Line label="commons.capacity" title={product.capacity.toString()} />
-            <Line label="commons.weight" title={product.capacity.toString()} />
-            <p className="text-center text-primary-main pt-5 uppercase font-bold">{t('commons.box-details')}</p>
-            <Line label="commons.units-per-box" title={product.units_per_box.toString()} />
-            <Line label="commons.capacity" title={product.capacity.toString()} />
-            <Line label="commons.width" title={product.capacity.toString()} />
-            <Line label="commons.length" title={product.capacity.toString()} />
-            <Line label="commons.height" title={product.capacity.toString()} />
-            <p className="text-center text-primary-main pt-5 uppercase font-bold">{t('commons.pallet-details')}</p>
-            <Line label="commons.units-per-pallet" title={product.capacity.toString()} />
-            <Line label="commons.base" title={product.capacity.toString()} />
-            <Line label="commons.capacity" title={product.capacity.toString()} />
-            <Line label="commons.height" title={product.capacity.toString()} />
-            <Line label="commons.weight" title={product.capacity.toString()} />
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-around w-1/2 lg:w-1/5 m-auto">
-        <Button text="commons.cancel" color="secondary" onClick={() => onClose()} outline></Button>
-        <Button text="commons.add" color="secondary" onClick={() => onAdd()}></Button>
-      </div>
-      <div className="absolute top-0 right-0 p-4 cursor-pointer" onClick={() => onClose()}>
-        X
-      </div>
-    </LayerOutOfRoot>
   );
 };
 
