660c1607839d7b407785fd7f2544afcabb256533
jauregui_ind <albertodomjauregui@hotmail.com>
2021-03-17 14:10:01 +0100

    initial year from moment

--- src/app/Config/duck/reducers.ts
+++ src/app/Config/duck/reducers.ts
@@ -2,8 +2,8 @@ import dayjs from 'dayjs';
 import { AnyAction } from 'redux';
 import { ConfigState, SET_YEAR } from './types';
 
-//const initialState = { year: parseInt(dayjs().format('YYYY')) };
-const initialState = { year: 2020 };
+const initialState = { year: parseInt(dayjs().format('YYYY')) };
+//const initialState = { year: 2020 };
 const configReducer = (state = initialState, { type, payload }: AnyAction): ConfigState => {
   switch (type) {
     case SET_YEAR: {
