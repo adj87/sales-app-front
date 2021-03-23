4f84e06237295f25707999e1b8935a1b7e1d2515
jauregui_ind <albertodomjauregui@hotmail.com>
2021-03-23 13:09:20 +0100

    choose inherit price

--- src/app/Fares/Modal/InheritFromModal.tsx
+++ src/app/Fares/Modal/InheritFromModal.tsx
@@ -17,39 +17,70 @@ interface InheritFromModalProps {
 }
 
 const InheritFromModal = ({ onCancel, onConfirm, fareToInheritFrom, fares, fetchFareWithCb }: InheritFromModalProps) => {
+  const [inheritPrices, setInheritPrices] = useState({
+    price_1: true,
+    price_2: false,
+    price_3: false,
+    price_4: false,
+  });
   const { submitForm, setValues, values, errors } = useFormik<IFare>({
     onSubmit: (values: IFare) => {
       // @ts-ignore
-      const fareLines = values.fare_lines.filter((fl: IFareLineWithCheck) => fl.checked);
+      let fareLines: IFareLine[] = values.fare_lines.filter((fl: IFareLineWithCheck) => fl.checked);
+      fareLines = fareLines.map((el: IFareLine) => {
+        return {
+          ...el,
+          price_1: inheritPrices.price_1 ? el.price_1 : 0,
+          price_2: inheritPrices.price_2 ? el.price_2 : 0,
+          price_3: inheritPrices.price_3 ? el.price_3 : 0,
+          price_4: inheritPrices.price_4 ? el.price_4 : 0,
+          to_sell: 1, // reset to_sell and to_charge to 1
+          to_charge: 1,
+        };
+      });
       onConfirm({ ...values, fare_lines: fareLines });
     },
     initialValues: defaultValues,
     validationSchema: validationSchemaInheritFrom,
   });
 
+  const onChangeInheritPrice = (name: string, checked: boolean) => setInheritPrices((oldState) => ({ ...oldState, [name]: checked }));
+
   return (
     <Modal centered onCancel={onCancel} onConfirm={submitForm}>
-      <SelectComponent
-        onChange={(name: string, fare: IFare) =>
-          fetchFareWithCb(fare.customer_id, (res: any) => {
-            const newFareLines = res.data.fare_lines.map((fl: IFareLine) => ({
-              ...fl,
-              checked: true,
-            }));
-            const fare = { ...res.data, fare_lines: newFareLines };
-            setValues(fare);
-          })
-        }
-        // @ts-ignore
-        options={fares}
-        // @ts-ignore
-        optionLabel={(fare: IFare) => fare.customer_name}
-        // @ts-ignore
-        optionValue={(fare: IFare) => fare.customer_id}
-        labelText="fares.form.inherit-from-another-customer-label"
-        value={values && values.customer_id}
-        name="inherit_from"
-      />
+      <div className="w-full sticky top-0 bg-white z-10 pb-10 border-b border-grey-400">
+        <SelectComponent
+          onChange={(name: string, fare: IFare) =>
+            fetchFareWithCb(fare.customer_id, (res: any) => {
+              const newFareLines = res.data.fare_lines.map((fl: IFareLine) => ({
+                ...fl,
+                checked: true,
+              }));
+              const fare = { ...res.data, fare_lines: newFareLines };
+              setValues(fare);
+            })
+          }
+          // @ts-ignore
+          options={fares}
+          // @ts-ignore
+          optionLabel={(fare: IFare) => fare.customer_name}
+          // @ts-ignore
+          optionValue={(fare: IFare) => fare.customer_id}
+          labelText="fares.form.inherit-from-another-customer-label"
+          value={values && values.customer_id}
+          name="inherit_from"
+        />
+        {values.customer_id && (
+          <div className="divide divide-y divide-grey-400 md:divide-y-8 pt-8">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+              <InputCheckBox value={inheritPrices.price_1} label={'commons.price-1'} onChange={onChangeInheritPrice} name={'price_1'} />
+              <InputCheckBox value={inheritPrices.price_2} label={'commons.price-2'} onChange={onChangeInheritPrice} name={'price_2'} />
+              <InputCheckBox value={inheritPrices.price_3} label={'commons.price-3'} onChange={onChangeInheritPrice} name={'price_3'} />
+              <InputCheckBox value={inheritPrices.price_4} label={'commons.price-4'} onChange={onChangeInheritPrice} name={'price_4'} />
+            </div>
+          </div>
+        )}
+      </div>
       {
         //@ts-ignore
         values.fare_lines.map((el: IFareLineWithCheck) => (
